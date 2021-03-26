87e664c0324e3fd43b8f68bd870079fd01e9a2a9
PBillingsby <philbillingsby@gmail.com>
2021-03-26 15:18:13 -0700

    Add form for spots. Add spot controller actions

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,3 +1,14 @@
 class SpotsController < ApplicationController
+  def new
+    @spot = Spot.new
+  end
 
+  def create
+    @spot = Spot.create(spot_params)
+    redirect_to spot_path(@spot)
+  end
+
+  def spot_params
+    params.require(:spot).permit(:name, :street_address, :city, :state, :country, :user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :spots, only: [:create, :show, :index, :update] do
+  resources :spots, only: [:new, :create, :show, :index, :update] do
     resources :ratings, only: [:create, :destroy]
   end
   root to: "users#index"
