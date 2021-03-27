c3133e3ec94914eb2c32b2c9bf01a66a8446cd61
PBillingsby <philbillingsby@gmail.com>
2021-03-27 08:40:59 -0700

    Add destroy action for spots

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,11 +1,12 @@
 class SpotsController < ApplicationController
   def new
     @spot = Spot.new
+    @rating = @spot.ratings.build
   end
 
   def create
-    spot = Spot.create(spot_params)
     binding.pry
+    spot = Spot.create(spot_params)
     if params[:spot][:image]
       image = Cloudinary::Uploader.upload(params[:spot][:image], options = {CLOUDINARY_URL: ENV["CLOUDINARY_NAME"]})
       spot.image_links.create(image_link: image["url"], user_id: current_user.id)
@@ -13,15 +14,22 @@ class SpotsController < ApplicationController
     redirect_to spot_path(spot)
   end
 
+  def index
+    @spots = Spot.all
+  end
+
   def show
     @spot = Spot.find(params[:id])
   end
 
   def destroy
-    binding.pry
+    spot = Spot.find(params[:id])
+    spot.delete
+    flash[:notice] = "#{spot.name} deleted"
+    redirect_to spots_path
   end
 
   def spot_params
-    params.require(:spot).permit(:name, :street_address, :city, :state, :country, :user_id, ratings_attributes: [:user_id, :spot_id, :rating, :description])
+    params.require(:spot).permit(:name, :street_address, :city, :state, :country, :user_id, { ratings_attributes: [:rating, :description] })
   end
 end
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,7 +1,7 @@
 class Spot < ApplicationRecord
   belongs_to :user
-  has_many :ratings
-  has_many :image_links
+  has_many :ratings, :dependent => :destroy
+  has_many :image_links, :dependent => :destroy
   
   accepts_nested_attributes_for :ratings
 end
